5da371236c81289e68e4fa750cd1e4f392354845
ScriptEd <scripted@ScriptEd-C02H6352DV7N.local>
2016-08-04 15:32:00 -0400

    inital

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#cristo").click(function(){
+  $("#heading").slideToggle("slow");
+});
+$("#cristo").click(function(){
+  $("#heading").fadeToggle();
+  $(".description").fadeToggle("slow");
+  $(".candy").fadeToggle(3000);
+});
+$("h1").css({"color":"#FF0066","font-size":"300%"});
